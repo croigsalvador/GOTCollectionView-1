1f9daa13f24741389f4ee1671756c9d6475bb6fa
Ricardo <odrakir@gmail.com>
2014-06-12 22:19:24 +0200

    CollectionView

--- GOTCollectionView/ViewController.m
+++ GOTCollectionView/ViewController.m
@@ -9,8 +9,9 @@
 #import "ViewController.h"
 #import "GotModel.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource>
 @property (nonatomic, strong) GotModel* modelo;
+@property (nonatomic, strong) UICollectionView* collectionView;
 @end
 
 @implementation ViewController
@@ -21,12 +22,37 @@
 
     self.modelo = [[GotModel alloc] init];
     [self.modelo cargaModelo];
+    
+    UICollectionViewFlowLayout* layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.itemSize = CGSizeMake(100, 100);
+    
+    self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    self.collectionView.dataSource = self;
+    [self.view addSubview:self.collectionView];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"celda"];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewWillLayoutSubviews
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewWillLayoutSubviews];
+    
+    self.collectionView.frame = self.view.bounds;
 }
 
+#pragma mark UICollectionView Datasource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell* cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"celda" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor redColor];
+    
+    return cell;
+}
 @end
