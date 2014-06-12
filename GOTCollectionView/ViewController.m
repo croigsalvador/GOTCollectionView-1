96067ff106c85fa2c8f26a9fc3d28eb61eeb256f
Ricardo <odrakir@gmail.com>
2014-06-12 22:06:47 +0200

    Initial commit

---
+++ GOTCollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GOTCollectionView
+//
+//  Created by Ricardo Sánchez Sotres on 12/06/14.
+//  Copyright (c) 2014 Ricardo Sanchez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
